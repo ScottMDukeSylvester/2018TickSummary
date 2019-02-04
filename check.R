90583d70abadbefb5faf67023fc82c8b8cb53631
Scott M. Duke-Sylvester <scottdukesylvester@gmail.com>
2019-02-04 17:41:03 -0600

    Added code to compare the sample locations against the map of Louisiana.

--- check.R
+++ check.R
@@ -12,6 +12,7 @@ assign("last.warning", NULL, envir = baseenv())
 library(tidyverse)
 library(rgdal)
 library(rgeos)
+library(sp)
 
 #############################################################
 # 
@@ -47,16 +48,28 @@ index = !is.na(data$lng) & (data$lng < -94 | -88 < data$lng)
 cat('check.R: Msg: Number of rows that have broken longitudes = ',sum(index),'\n')
 print(data[index, ])
 
-#index = data$lat < 28 | 33 < data$lat
-#cat('check.R: Msg: Number of rows that have broken latitudes = ',sum(index),'\n')
+index = is.na(data$lat)
+cat('check.R: Msg: Number of rows that have no latitude      = ',sum(index),'\n')
+index = !is.na(data$lat) & (data$lat < 28 | 33 < data$lat)
+cat('check.R: Msg: Number of rows that have broken latitudes = ',sum(index),'\n')
+print(data[index, ])
 
-if ( F ) {
-    # Load the states shapefile
-    filename = list( dsn = './SpatialData/US States', layer = 'cb_2017_us_state_5m')
-    stateSF  = readOGR( dsn = filename$dsn , layer = filename$layer )
-    
-    # Turn the sampling location data into a shapefile object
-    dataSF = subset( data, !is.na(lng) & !is.na(lat) )
-    coordinates(dataSF) = ~lng + lat
-    projection(dataSF) = "+proj=longlat +datum=WGS84 +no_defs"
-} 
+# Drop locations with broken or missing coordinates for now
+cat('check.R: Msg: Dropping rows with broken or missing coordinates\n')
+data = subset(data, !is.na(data$lng) & (-94 < data$lng & data$lng < -88) )
+data = subset(data, !is.na(data$lat) & ( 28 < data$lat & data$lat < 33 ) )
+
+# Load the states shapefile
+filename = list( dsn = './SpatialData/US States', layer = 'cb_2017_us_state_5m')
+stateSF  = readOGR( dsn = filename$dsn , layer = filename$layer )
+
+# Turn the sampling location data into a shapefile object
+dataSF = subset( data, !is.na(lng) & !is.na(lat) )
+coordinates(dataSF) = ~lng + lat
+proj4string(dataSF) = "+proj=longlat +datum=WGS84 +no_defs"
+
+# Reproject the data to the same system as the stateSF shapefile
+dataSF = spTransform(dataSF, proj4string(stateSF) )
+
+ret = over(dataSF, stateSF)
+index = is.na(ret$NAME) | ret$NAME != 'Louisiana'
